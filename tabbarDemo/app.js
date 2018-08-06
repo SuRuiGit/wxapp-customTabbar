13d45eabad28f037bde368f8391d49c3eefffced
苏蕊 <surui@qijiayoudao.com>
2018-08-06 11:38:00 +0800

    finish custom tabbar demo

---
+++ tabbarDemo/app.js
@@ -0,0 +1,87 @@
+//app.js
+App({
+  onLaunch: function () {
+    //隐藏系统tabbar
+    wx.hideTabBar();
+    //获取设备信息
+    this.getSystemInfo();
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  getSystemInfo: function () {
+    let t = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        t.globalData.systemInfo = res;
+      }
+    });
+  },
+  editTabbar: function () {
+    let tabbar = this.globalData.tabBar;
+    let currentPages = getCurrentPages();
+    let _this = currentPages[currentPages.length - 1];
+    let pagePath = _this.route;
+    (pagePath.indexOf('/') != 0) && (pagePath = '/' + pagePath);
+    for (let i in tabbar.list) {
+      tabbar.list[i].selected = false;
+      (tabbar.list[i].pagePath == pagePath) && (tabbar.list[i].selected = true);
+    }
+    _this.setData({
+      tabbar: tabbar
+    });
+  },
+  globalData: {
+    systemInfo: null,//客户端设备信息
+    userInfo: null,
+    tabBar: {
+      "backgroundColor": "#ffffff",
+      "color": "#979795",
+      "selectedColor": "#1c1c1b",
+      "list": [
+        {
+          "pagePath": "/page/index/index",
+          "iconPath": "icon/icon_home.png",
+          "selectedIconPath": "icon/icon_home_HL.png",
+          "text": "首页"
+        },
+        {
+          "pagePath": "/page/myRelease/index",
+          "iconPath": "icon/icon_release.png",
+          "isSpecial": true,
+          "text": "发布"
+        },
+        {
+          "pagePath": "/page/mine/index",
+          "iconPath": "icon/icon_mine.png",
+          "selectedIconPath": "icon/icon_mine_HL.png",
+          "text": "我的"
+        }
+      ]
+    }
+  }
+})
---
+++ tabbarDemo/tabbarComponent/tabbar.js
@@ -0,0 +1,51 @@
+// tabBarComponent/tabBar.js
+const app = getApp();
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    tabbar: {
+      type: Object,
+      value: {
+        "backgroundColor": "#ffffff",
+        "color": "#979795",
+        "selectedColor": "#1c1c1b",
+        "list": [
+          {
+            "pagePath": "page/index/index",
+            "iconPath": "icon/icon_home.png",
+            "selectedIconPath": "icon/icon_home_HL.png",
+            "text": "首页"
+          },
+          {
+            "pagePath": "page/myRelease/index",
+            "iconPath": "icon/icon_release.png",
+            "isSpecial": true,
+            "text": "发布"
+          },
+          {
+            "pagePath": "page/mine/index",
+            "iconPath": "icon/icon_mine.png",
+            "selectedIconPath": "icon/icon_mine_HL.png",
+            "text": "我的"
+          }
+        ]
+      }
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    isIphoneX: app.globalData.systemInfo.model == "iPhone X" ? true : false,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
